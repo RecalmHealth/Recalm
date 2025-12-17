340ae11993ba3b25855d39f7440ae3fa3ea0f348
Daffa Yusuf Mahendra <daffayusuf075@gmail.com>
2025-12-17 22:01:20 +0700

    fix button profile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,47 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Recalm')</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     @vite('resources/sass/app.scss')
-    <style>
-        body {
-            background-color: #eef2f6;
-            overflow: hidden; 
-            height: 100vh;
-        }
-        .main-content {
-            width: 100%;
-            height: 100vh;
-            overflow-y: scroll; 
-            padding: 0;
-        }
-    </style>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 </head>
 <body>
-    <div class="d-flex h-100">
+<div class="d-flex">
+    @include('review.components.sidebar')
 
+    <div class="flex-grow-1 d-flex flex-column min-vh-100 overflow-auto">
+        @include('review.components.navbar-app')
 
-        @include('review.components.sidebar')
-        <div class="main-content">
-            @include('review.components.navbar-app')
+        {{-- Konten utama --}}
+        <main class="flex-grow-1 p-3 p-md-4">
+            @yield('content')
 
-            <div class="p-4">
-                @yield('content')
-            </div>
-        </div>
+        </main>
     </div>
+  </div>
 
-    {{-- @vite('resources/js/app.js') --}}
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
-            var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
-                return new bootstrap.Dropdown(dropdownToggleEl)
-            })
-        });
-    </script>
+  @vite('resources/js/app.js')
 </body>
 </html>
